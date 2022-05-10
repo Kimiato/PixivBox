60b807f683b33b5f5c3fbc96a11f74a7f7466de5
Kimiato <853447650@qq.com>
2022-05-11 01:14:15 +0800

    style: flake8

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 import sys
 
 from api import app
-from api.core.pixiv import pixiv
 
 
 if __name__ == '__main__':
@@ -9,4 +8,3 @@ if __name__ == '__main__':
     debug = True if '-debug' in args else False
 
     app.run(host='0.0.0.0', port=8000, debug=debug)
-    app.add_task(pixiv._login(app))
