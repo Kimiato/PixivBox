f30dea1f30c31c988bef9917a1ba1ef3524d8b25
Kimiato <853447650@qq.com>
2022-05-12 01:59:51 +0800

    feat: 添加瀑布流前端展示

--- api/app.py
+++ api/app.py
@@ -3,4 +3,6 @@ from sanic import Sanic
 from .core.pixiv import refresh_token
 
 app = Sanic('pixiv-box')
+app.static('/', './static')
+app.static('/', './static/index.html')
 app.add_task(refresh_token)
