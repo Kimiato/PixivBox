cecb7e96f276d1c6863389989f90a32465623e0a
Kimiato <853447650@qq.com>
2022-05-12 23:34:26 +0800

    feat: add cors

--- api/app.py
+++ api/app.py
@@ -1,8 +1,10 @@
 from sanic import Sanic
 
 from .core.pixiv import refresh_token
+from sanic_cors import CORS
 
 app = Sanic('pixiv-box')
+CORS(app)
 app.static('/', './static')
 app.static('/', './static/index.html')
 app.add_task(refresh_token)
